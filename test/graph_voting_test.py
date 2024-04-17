d59c1f3151772c9fc705ce1c30555555487eb9c5
adam_b3n3s <ad.benes@gmail.com>
2024-04-17 20:01:49 +0200

    Tests added

--- test/graph_voting_test.py
+++ test/graph_voting_test.py
@@ -12,7 +12,7 @@ from collections import Counter
 from src.vote_rules.brute_force.graph_voting import graph_voting
 
 class graph_voting_test(unittest.TestCase):
-    def test_plurality1(self):
+    def test_graph_voting1(self):
         votes = [["a", "b", "c", "d"], ["d", "b", "c", "a"], ["d", "c", "a", "b"], ["d", "c", "a", "b"], ["d", "c", "a", "b"]]
         candidates = ["a", "b", "c", "d"]
         Gv = graph_voting()
@@ -20,6 +20,56 @@ class graph_voting_test(unittest.TestCase):
         G = graph_voting.voting(votes, G)
         result = graph_voting.find_winner(G)
         expected_output_1 = "d"  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_graph_voting2(self):
+        votes = [["B", "C", "D", "A"], ["A", "D", "C", "B"], ["B", "A", "D", "C"], ["A", "B", "C", "D"], ["A", "C", "D", "B"]]
+        candidates = ["A", "B", "C", "D"]
+        Gv = graph_voting()
+        G = graph_voting.inicilize_complete_graph(candidates)
+        G = graph_voting.voting(votes, G)
+        result = graph_voting.find_winner(G)
+        expected_output_1 = "A"  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_graph_voting3(self):
+        votes = [["e", "b", "d", "a", "c"], ["c", "d", "a", "e", "b"], ["c", "a", "b", "d", "e"], ["a", "e", "c", "b", "d"], ["e", "a", "c", "b", "d"]]
+        candidates = ["a", "b", "c", "d", "e"]
+        Gv = graph_voting()
+        G = graph_voting.inicilize_complete_graph(candidates)
+        G = graph_voting.voting(votes, G)
+        result = graph_voting.find_winner(G)
+        expected_output_1 = "a"  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_graph_voting4(self):
+        votes = [["a"], ["a"], ["a"], ["a"]]
+        candidates = ["a"]
+        Gv = graph_voting()
+        G = graph_voting.inicilize_complete_graph(candidates)
+        G = graph_voting.voting(votes, G)
+        result = graph_voting.find_winner(G)
+        expected_output_1 = "a"  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_graph_voting5(self):
+        votes = [["A", "B", "C", "D", "E", "F", "G"], ["A", "B", "C", "D", "E", "F", "G"], ["A", "B", "C", "D", "E", "F", "G"], ["A", "B", "C", "D", "E", "F", "G"], ["A", "B", "C", "D", "E", "F", "G"]]
+        candidates = ["A", "B", "C", "D", "E", "F", "G"]
+        Gv = graph_voting()
+        G = graph_voting.inicilize_complete_graph(candidates)
+        G = graph_voting.voting(votes, G)
+        result = graph_voting.find_winner(G)
+        expected_output_1 = "A"  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_graph_voting6(self):
+        votes = [["D", "E", "F", "A", "B", "G", "C"], ["E", "C", "D", "A", "F", "G", "B"], ["G", "C", "E", "F", "D", "A", "B"], ["E", "F", "A", "B", "C", "D", "G"], ["D", "E", "F", "A", "B", "G", "C"]]
+        candidates = ["A", "B", "C", "D", "E", "F", "G"]
+        Gv = graph_voting()
+        G = graph_voting.inicilize_complete_graph(candidates)
+        G = graph_voting.voting(votes, G)
+        result = graph_voting.find_winner(G)
+        expected_output_1 = "E"  # Expected output dictionary
         self.assertEqual(result, expected_output_1)
         
         
--- test/plurality_test.py
+++ test/plurality_test.py
@@ -15,11 +15,83 @@ from src.vote_rules.brute_force.vector import Vector
 
 class plurality_test(unittest.TestCase):
     def test_plurality1(self):
-        votes = [[1, 1, 1, 0, 0], [0, 1, 1, 0, 1], [0, 0, 0, 1, 1], [1, 0, 1, 1, 0], [0, 0, 1, 1, 1], [1, 1, 1, 0, 0]]
+        votes = [[1, 1, 1, 0, 0], [0, 1, 1, 0, 1], [1, 0, 0, 1, 1], [1, 0, 1, 1, 0], [0, 0, 1, 1, 1], [1, 1, 1, 0, 0]]
         nr_of_candidates = 5
         Pl = Plurality(nr_of_candidates)
         result = Pl.votes_process(votes)
-        expected_output_1 = str(Vector([3, 3, 5, 3, 3]))  # Expected output dictionary
+        expected_output_1 = str(Vector([4, 3, 5, 3, 3]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality2(self):
+        votes = [[0, 0, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 1], [0, 0, 1, 0, 0], [0, 0, 0, 0, 1], [0, 0, 1, 0, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([0, 1, 3, 0, 2]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality3(self):
+        votes = [[1, 0, 1, 0, 0], [0, 0, 1, 0, 1], [0, 0, 0, 1, 1], [1, 0, 0, 1, 0], [0, 0, 0, 1, 1], [1, 1, 0, 0, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([3, 1, 2, 3, 3]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality4(self):
+        votes = [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([6, 6, 6, 6, 6]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality5(self):
+        votes = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([0, 0, 0, 0, 0]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality_veto1(self):
+        votes = [[-1, -1, -1, 0, 0], [0, -1, -1, 0, -1], [0, 0, -1, -1, -1], [-1, 0, -1, -1, 0], [0, 0, -1, -1, -1], [-1, -1, -1, 0, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([-3, -3, -6, -3, -3]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality_veto2(self):
+        votes = [[1, 0, 0, -1, -1], [0, -1, -1, 0, -1], [0, -1, 1, 0, -1], [0, -1, 0, 1, -1], [0, 0, -1, -1, 1], [-1, 0, 1, -1, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([0, -3, 0, -2, -3]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality_veto3(self):
+        votes = [[0, -1, 0, 0, 0], [0, -1, 0, 0, 0], [0, 0, 0, -1, 0], [-1, 0, 0, 0, 0], [0, 0, -1, 0, 0], [-1, 0, 0, 0, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([-2, -2, -1, -1, 0]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality_veto4(self):
+        votes = [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([-6, -6, -6, -6, -6]))  # Expected output dictionary
+        self.assertEqual(result, expected_output_1)
+        
+    def test_plurality_veto5(self):
+        votes = [[1, -1, 1, 0, 0], [0, -1, 1, 0, 1], [0, 0, -1, 1, 1], [-1, 0, 1, 1, 0], [0, 0, 1, 1, -1], [1, -1, 1, 0, 0]]
+        nr_of_candidates = 5
+        Pl = Plurality(nr_of_candidates)
+        result = Pl.votes_process(votes)
+        expected_output_1 = str(Vector([1, -3, 4, 3, 1]))  # Expected output dictionary
         self.assertEqual(result, expected_output_1)
         
         
