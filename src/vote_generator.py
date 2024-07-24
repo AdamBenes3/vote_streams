4a5eb9c208923d969072d3b8523ccc90da74fe45
adam_b3n3s <ad.benes@gmail.com>
2024-07-24 13:14:38 +0200

    Minor changes - tryed to speed up (didnt end up good yet)

--- src/vote_generator.py
+++ src/vote_generator.py
@@ -17,12 +17,3 @@ def generate_random_number_exponential_distribution(lmbd : int, size : int = 1) 
     if size == 1:
         return random_numbers_int[0]
     return random_numbers_int.tolist()
-
-def generate_random_number_normal_distribution(mean : int, size : int = 1) -> Union[int, List]:
-    # Generate random numbers from a normal distribution
-    random_numbers = np.random.normal(loc=mean, scale=1, size=size)
-    random_numbers_int = random_numbers.round().astype(int)
-    if size == 1:
-        return random_numbers_int[0]
-    return random_numbers_int.tolist()
-
